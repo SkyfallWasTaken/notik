2e7526160653ecfa21dcfe133d36010954165ba8
SkyfallWasTaken <mahadkalam1@proton.me>
2024-04-12 11:02:06 +0100

    Big bang

---
+++ src/lib/components/new/NewCard.svelte
@@ -0,0 +1,10 @@
+<div class="card">
+	<header class="card-header p-4 text-left">(header)</header>
+	<hr class="opacity-50" />
+	<section class="p-4">
+		<div class="space-y-2">
+			<textarea class="textarea" placeholder="Front" required />
+			<textarea class="textarea" placeholder="Back" required />
+		</div>
+	</section>
+</div>
---
+++ src/routes/new/+page.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { PencilLine, GraduationCap } from 'lucide-svelte';
+	import { Autocomplete, popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings, AutocompleteOption } from '@skeletonlabs/skeleton';
+	import NewCard from '$lib/components/new/NewCard.svelte';
+
+	let deckSubject = '';
+	let popupSettings: PopupSettings = {
+		event: 'focus-click',
+		target: 'popupAutocomplete',
+		placement: 'bottom'
+	};
+
+	let availableSubjects: AutocompleteOption<string>[] = [
+		{
+			label: 'Computer Science',
+			value: 'computerscience',
+			keywords: 'cs, computing, programming, coding'
+		},
+		{ label: 'English', value: 'english', keywords: 'english, language, literature' },
+		{ label: 'Maths', value: 'maths', keywords: 'math, mathematics, algebra, geometry, calculus' }
+	];
+
+	function onSubjectSelect(event: CustomEvent<AutocompleteOption<string>>) {
+		deckSubject = event.detail.value;
+	}
+</script>
+
+<div class="container mx-auto flex justify-center">
+	<div class="space-y-5 text-center flex flex-col">
+		<div class="text-left space-y-2">
+			<h1 class="text-3xl font-semibold">Create new deck</h1>
+			<label for="deckName">
+				<span class="sr-only">Deck name</span>
+				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+					<div class="input-group-shim"><PencilLine /></div>
+					<input type="text" placeholder="e.g. 'German - Essen'" id="deckName" />
+				</div>
+			</label>
+
+			<label for="deckSubject">
+				<span class="sr-only">Subject</span>
+				<div class="input-group input-group-divider grid-cols-[auto_1fr_auto]">
+					<div class="input-group-shim"><GraduationCap /></div>
+					<input
+						class="input autocomplete"
+						type="search"
+						name="autocomplete-search"
+						bind:value={deckSubject}
+						placeholder="Search..."
+						use:popup={popupSettings}
+					/>
+					<div data-popup="popupAutocomplete">
+						<Autocomplete
+							bind:input={deckSubject}
+							options={availableSubjects}
+							on:selection={onSubjectSelect}
+						/>
+					</div>
+				</div>
+			</label>
+		</div>
+
+		<hr class="opacity-50" />
+
+		<NewCard />
+	</div>
+</div>
